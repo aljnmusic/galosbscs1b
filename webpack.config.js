8dbe91cdb286fcec1a315df7960e949ad05548f7
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-08-29 22:19:47 +0800

    initial commit for todo

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './bscs1b/index.js',
+  entry: './src/index.js', // Entry point for your application
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Output directory
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
+        test: /\.css$/, // Process CSS files
+        use: [
+          'style-loader', // Injects styles into DOM
+          'css-loader',   // Resolves CSS imports
+          'postcss-loader' // Processes CSS with PostCSS
+        ],
       },
     ],
   },
+  mode: 'production', // Set the mode to production
 };
